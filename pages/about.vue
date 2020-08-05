d034a49bb561460ad2e5fde245ea38acf818d434
Jamie <jamiewong63@googlemail.com>
2020-08-05 15:13:11 +0100

    add about route to application

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>This is an app that displays dad jokes called from an API</p>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style>
+</style>
