a4ae456a0d3b284e07d3cb7a1b8229870a81f50f
Jamie <jamiewong63@googlemail.com>
2020-08-06 13:09:05 +0100

    random joke page that shows one random joke

---
+++ pages/RandomJoke/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Random Joke Page</h1>
+  </div>
+</template>
+
+<script>
+import axios from '../../components/Joke';
+
+export default {};
+</script>
+
+<style>
+
+</style>
